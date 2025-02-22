4a112e8ca81055d92d32c5c7f0132ec5ce4697a5
KeineSecrets <miethe.max@web.de>
2025-02-22 23:57:47 +0100

    Refactor resource pack handling by introducing ResourcePackRequestBuilder for improved readability and functionality

--- src/main/java/eu/revanox/skyblock/listener/PlayerResourcePackListener.java
+++ src/main/java/eu/revanox/skyblock/listener/PlayerResourcePackListener.java
@@ -1,5 +1,6 @@
 package eu.revanox.skyblock.listener;
 
+import eu.revanox.skyblock.util.builder.ResourcePackRequestBuilder;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.minimessage.MiniMessage;
@@ -14,10 +15,19 @@ public class PlayerResourcePackListener implements Listener {
     public void onPlayerLogin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
 
-        String resourcePack = "https://download.mc-packs.net/pack/ab9654d76796ed80d403b0eafe81e501cf615d9c.zip";
-        String sha1ResourcePack = "ab9654d76796ed80d403b0eafe81e501cf615d9c";
-        player.setResourcePack(player.getUniqueId(), resourcePack, sha1ResourcePack,
-                MiniMessage.miniMessage().deserialize("<gradient:#E2A574:#E0EF50>Blockarion</gradient>")
-                        .appendNewline().append(Component.text("Bitte akzeptiere unser Texturenpack, um spielen zu können!", NamedTextColor.RED)), true);
+//        String resourcePack = "https://download.mc-packs.net/pack/ab9654d76796ed80d403b0eafe81e501cf615d9c.zip";
+//        String sha1ResourcePack = "ab9654d76796ed80d403b0eafe81e501cf615d9c";
+//        player.setResourcePack(player.getUniqueId(), resourcePack, sha1ResourcePack,
+//                MiniMessage.miniMessage().deserialize("<gradient:#E2A574:#E0EF50>Blockarion</gradient>")
+//                        .appendNewline().append(Component.text("Bitte akzeptiere unser Texturenpack, um spielen zu können!", NamedTextColor.RED)), true);
+
+        ResourcePackRequestBuilder.create()
+                .player(player)
+                .url("https://download.mc-packs.net/pack/ab9654d76796ed80d403b0eafe81e501cf615d9c.zip")
+                .kickMessage(
+                        MiniMessage.miniMessage().deserialize("<gradient:#E2A574:#E0EF50>Blockarion</gradient>"),
+                        Component.text("Bitte akzeptiere unser Texturenpack, um spielen zu können!", NamedTextColor.RED)
+                )
+                .send();
     }
 }
---
+++ src/main/java/eu/revanox/skyblock/util/builder/ResourcePackRequestBuilder.java
@@ -0,0 +1,59 @@
+package eu.revanox.skyblock.util.builder;
+
+import net.kyori.adventure.resource.ResourcePackCallback;
+import net.kyori.adventure.resource.ResourcePackInfo;
+import net.kyori.adventure.resource.ResourcePackRequest;
+import net.kyori.adventure.text.Component;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerKickEvent;
+
+import java.net.URI;
+import java.util.Arrays;
+
+public class ResourcePackRequestBuilder {
+
+    String url;
+    Component kickMessage;
+    Player player;
+
+    private ResourcePackRequestBuilder() {
+
+    }
+
+    public static ResourcePackRequestBuilder create() {
+        return new ResourcePackRequestBuilder();
+    }
+
+    public ResourcePackRequestBuilder url(String url) {
+        this.url = url;
+        return this;
+    }
+
+    public ResourcePackRequestBuilder kickMessage(Component... kickMessage) {
+        this.kickMessage = Component.text().append(Arrays.asList(kickMessage)).build();
+        return this;
+    }
+
+    public ResourcePackRequestBuilder player(Player player) {
+        this.player = player;
+        return this;
+    }
+
+    public void send() {
+        ResourcePackRequest request = ResourcePackRequest.resourcePackRequest()
+                .required(true)
+                .packs(
+                        ResourcePackInfo.resourcePackInfo(player.getUniqueId(), URI.create(this.url), getHashFromUrl(this.url))
+                ).callback(ResourcePackCallback.onTerminal(((uuid, audience) -> {
+            //say thanks
+        }), ((uuid, audience) -> {
+            this.player.kick(this.kickMessage, PlayerKickEvent.Cause.RESOURCE_PACK_REJECTION);
+        }))).build();
+        this.player.sendResourcePacks(request);
+    }
+
+    private String getHashFromUrl(String url) {
+        return url.substring(url.lastIndexOf("/") + 1).replaceAll(".zip", "");
+    }
+
+}
