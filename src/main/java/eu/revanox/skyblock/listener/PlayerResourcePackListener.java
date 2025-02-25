a07dd02df25fbecd41d8a7e8464a7c792cc0f12a
KeineSecrets <miethe.max@web.de>
2025-02-25 12:46:00 +0100

    Refactor player login resource pack handling for improved clarity and localization

--- src/main/java/eu/revanox/skyblock/listener/PlayerResourcePackListener.java
+++ src/main/java/eu/revanox/skyblock/listener/PlayerResourcePackListener.java
@@ -16,18 +16,12 @@ public class PlayerResourcePackListener implements Listener {
     public void onPlayerLogin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
 
-//        String resourcePack = "https://download.mc-packs.net/pack/ab9654d76796ed80d403b0eafe81e501cf615d9c.zip";
-//        String sha1ResourcePack = "ab9654d76796ed80d403b0eafe81e501cf615d9c";
-//        player.setResourcePack(player.getUniqueId(), resourcePack, sha1ResourcePack,
-//                MiniMessage.miniMessage().deserialize("<gradient:#E2A574:#E0EF50>Blockarion</gradient>")
-//                        .appendNewline().append(Component.text("Bitte akzeptiere unser Texturenpack, um spielen zu können!", NamedTextColor.RED)), true);
-
         ResourcePackRequestBuilder.create()
                 .player(player)
                 .url(SkyBlockPlugin.instance().getResourcePackConfiguration().getUrl())
                 .kickMessage(
                         MiniMessage.miniMessage().deserialize("<gradient:#E2A574:#E0EF50>Blockarion</gradient>"),
-                        Component.text("Bitte akzeptiere unser Texturenpack, um spielen zu können!", NamedTextColor.RED)
+                        Component.text("Bitte akzeptiere unser Texturenpaket, um spielen zu können!", NamedTextColor.RED)
                 )
                 .send();
     }
