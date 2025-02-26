962b8d6dcc56940b33bed357df32cfcda6b510cc
pesoa <sebastianf0@web.de>
2025-02-26 14:46:33 +0100

    Main SkyBlock commit

--- src/main/java/mc/skyblock/plugin/tag/model/Tags.java
+++ src/main/java/mc/skyblock/plugin/tag/model/Tags.java
@@ -2,6 +2,7 @@ package mc.skyblock.plugin.tag.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import mc.skyblock.plugin.util.Rarity;
 import mc.skyblock.plugin.util.ResourceIcons;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
@@ -15,15 +16,15 @@ import java.util.List;
 public enum Tags {
 
     // MiniMessage.miniMessage().deserialize("<#D6557F>I <#EA2525>❤ <#D6557F>SkyBlock")
-    I_LOVE_SKYBLOCK(Component.text("§r" + ResourceIcons.SUMMER_2025_TITLE.unicode()), TagRarity.COMMON, 10),
+    I_LOVE_SKYBLOCK(Component.text("§r" + ResourceIcons.SUMMER_2025_TITLE.unicode()), Rarity.MYTHIC, 10),
     GELD_GIGANT(
-            MiniMessage.miniMessage().deserialize("<gradient:#FFD900:#E9B96E> Geldgigant </gradient><reset>"), TagRarity.EPIC, 50000),
-    RAINBOW(MiniMessage.miniMessage().deserialize("<red>🔥 <rainbow>Regenbogen</rainbow> <red>🔥"), TagRarity.RARE, 25000),
-    DONATOR(MiniMessage.miniMessage().deserialize("<#E43A96>★ <gradient:#B308FB:#C2F68E>Unterstüt</gradient><gradient:#C2F68E:#C2F68E>zer</gradient> <#E43A96>★"), TagRarity.EXCLUSIVE, -1),
-    FOUNDER_LEGEND(MiniMessage.miniMessage().deserialize("<b><gradient:#FF006F:#FF9B00:#00F9FF:#8000FF>◆ Gründerlegende ◆</gradient>"), TagRarity.EXCLUSIVE, -1);
+            MiniMessage.miniMessage().deserialize("<gradient:#FFD900:#E9B96E> Geldgigant </gradient><reset>"), Rarity.EPIC, 50000),
+    RAINBOW(MiniMessage.miniMessage().deserialize("<red>🔥 <rainbow>Regenbogen</rainbow> <red>🔥"), Rarity.RARE, 25000),
+    DONATOR(MiniMessage.miniMessage().deserialize("<#E43A96>★ <gradient:#B308FB:#C2F68E>Unterstüt</gradient><gradient:#C2F68E:#C2F68E>zer</gradient> <#E43A96>★"), Rarity.UNCOMMON, -1),
+    FOUNDER_LEGEND(MiniMessage.miniMessage().deserialize("<b><gradient:#FF006F:#FF9B00:#00F9FF:#8000FF>◆ Gründerlegende ◆</gradient>"), Rarity.EXCLUSIVE, -1);
 
     Component tagText;
-    TagRarity rarity;
+    Rarity rarity;
     double price;
 
     public static List<Tags> sortByRarity() {
