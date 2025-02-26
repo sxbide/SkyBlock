8278fbb2c0d8e5bd5d73d4643d7073850e9128a0
pesoa <sebastianf0@web.de>
2025-02-26 23:37:04 +0100

    Main SkyBlock commit

---
+++ src/main/java/mc/skyblock/plugin/util/CustomSound.java
@@ -0,0 +1,12 @@
+package mc.skyblock.plugin.util;
+
+import lombok.AllArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Accessors(fluent = true)
+@AllArgsConstructor
+public enum CustomSound {
+
+
+
+}
