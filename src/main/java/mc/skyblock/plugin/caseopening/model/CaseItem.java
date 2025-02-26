a70ce4eb0bffa621a36dbda3857c30fdfe34b1fd
KeineSecrets <miethe.max@web.de>
2025-02-26 14:43:33 +0100

    Add rarity determination method to CaseItem for item chance evaluation

--- src/main/java/mc/skyblock/plugin/caseopening/model/CaseItem.java
+++ src/main/java/mc/skyblock/plugin/caseopening/model/CaseItem.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
+import mc.skyblock.plugin.util.Rarity;
 import org.bukkit.inventory.ItemStack;
 
 @Getter
@@ -16,6 +17,34 @@ public class CaseItem {
 
     ItemStack itemStack;
     double chance;
+
+    public Rarity getRarity() {
+        double maxExclusiveChance = 0.2;
+        if (chance < maxExclusiveChance) {
+            return Rarity.EXCLUSIVE;
+        }
+        double maxLegendaryChance = 5.0;
+        if (chance < maxLegendaryChance) {
+            return Rarity.LEGENDARY;
+        }
+        double maxMythicChance = 10.0;
+        if (chance < maxMythicChance) {
+            return Rarity.MYTHIC;
+        }
+        double maxEpicChance = 15.0;
+        if (chance < maxEpicChance) {
+            return Rarity.EPIC;
+        }
+        double maxRareChance = 20.0;
+        if (chance < maxRareChance) {
+            return Rarity.RARE;
+        }
+        double maxUncommonChance = 30.0;
+        if (chance < maxUncommonChance) {
+            return Rarity.UNCOMMON;
+        }
+        return Rarity.COMMON;
+    }
 
 
 }
